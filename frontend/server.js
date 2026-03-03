8b5af392cb45e3e0f642430c2312f5c7966db2a9
Julio Lima Casagrande <JLCASAGRANDE@ctgpar.ctgbr.com.br>
2026-03-03 06:22:57 -0300

    fix: listen on all interfaces

--- frontend/server.js
+++ frontend/server.js
@@ -1,13 +1,11 @@
-﻿const express = require('express');
+const express = require('express');
 const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Servir arquivos estaticos da pasta dist
 app.use(express.static(path.join(__dirname, 'dist')));
 
-// Para SPA - redirecionar todas as rotas para index.html
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
